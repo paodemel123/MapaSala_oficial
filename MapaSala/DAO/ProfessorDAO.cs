d2dad6c294995a27ca26360e97f28c1c846d3c5f
paodemel123 <drianymia1@gmail.com>
2024-08-05 13:42:36 -0300

    oii

---
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -0,0 +1,27 @@
+﻿public class ProfessorDAO
+{
+    private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
+    private SqlConnection Conexao;
+    public ProfessorDAO()
+    {
+        Conexao = new SqlConnection(LinhaConexao);
+    }
+    public void Inserir(ProfessoresEntidade professor)
+    {
+        Conexao.Open();
+        string query = "Insert into Professores (Nome, Apelido) Values (@nome, @apelido)";
+        SqlCommand comando = new SqlCommand(query, Conexao);
+        SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+        SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+        comando.Parameters.Add(parametro1);
+        comando.Parameters.Add(parametro2);
+        comando.ExecuteNonQuery();
+        Conexao.Close();
+
+
+
+    }
+
+
+}
+}
