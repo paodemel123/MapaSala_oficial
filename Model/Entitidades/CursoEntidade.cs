c9382888fd52953c9182bf37d5224bb0deac5fe4
paodemel123 <drianymia1@gmail.com>
2024-08-27 10:46:47 -0300

    alterações no cursos entidade, precisa refazer frm para o DAO

---
+++ Model/Entitidades/CursoEntidade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entitidades
+{
+    public class CursoEntidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turno { get; set; }
+        public string Sigla { get; set; }
+        public bool Ativo { get; set; }
+
+        public object[] Linha()
+        {
+            return new object[] { Id, Nome, Sigla };
+        }
+    }
+}
